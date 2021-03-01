522de9c203d1487b55d10ad4254069b2950a19e3
Mykel <songsongha@gmail.com>
2021-03-01 16:16:07 -0700

    added body-parser and routing

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+	users:[
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@email.com',
+			password: 'cookies',
+			entries: '0',
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Sally',
+			email: 'sally@email.com',
+			password: 'bananas',
+			entries: '0',
+			joined: new Date()
+		}
+
+	]
+}
+
+app.get('/', (req, res) => {
+	res.send(database.users[0].email);
+})
+
+app.post('/signin', (req, res) => {
+	if(req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password) {
+		res.json('success');
+	}else{
+		res.status(400).json('error logging in');
+	}
+})
+
+app.listen(3000, ()=> {
+	console.log('app is running on port 3000');
+})
+
+/*
+/ --> res = this is working
+/signin --> post = usccess/fail
+/register --> post = user
+/profile/:userID ==> get = user
+/image --> put upddated User
+*/
+
