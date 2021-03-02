7c65271b520d7ee3b812e0dfaa9e015497716726
Mykel <songsongha@gmail.com>
2021-03-02 13:35:26 -0700

    added register, profile/:id, and image routes

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-	res.send(database.users[0].email);
+	res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
@@ -39,6 +39,51 @@ app.post('/signin', (req, res) => {
 	}else{
 		res.status(400).json('error logging in');
 	}
+})
+
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: '0',
+		joined: new Date()
+	})
+	res.json(database.users[database.users.length-1]);
+})
+
+
+app.get('/profile/:id', (req, res) => {
+	const {id} = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		} 
+	})
+	
+	if (!found) {
+		res.status(404).json('no such user');
+		}
+})
+
+app.post('/image', (req, res) =>{
+	const {id} = req.body;
+	let found = false;
+	database.users.forEach(user => {
+	if (user.id === id) {
+		found = true;
+		user.entries++
+		return res.json(user.entries);
+	} 
+	})
+	
+	if (!found) {
+		res.status(404).json('no such user');
+		}
 })
 
 app.listen(3000, ()=> {
