dc86fec255e43b17d2ba981311169906f8ecd0f1
Mykel <songsongha@gmail.com>
2021-04-01 16:29:58 -0600

    connected signin route to database with bcrypt hash

--- script.js
+++ script.js
@@ -48,41 +48,53 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-	// bcrypt.compare(hash, function(err, res) {
-	// 	//res == true
-	// });
-	// bcrypt.compare("", hash, function(err, res) {
-	// 	// res = false
-	// });
-
-	if(req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-	}else{
-		res.status(400).json('error logging in');
-	}
+	db.select('email','hash').from('login')
+	.where('email', '=', req.body.email)
+	.then(data => {
+		const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+		if (isValid) {
+			return db.select('*').from('users')
+				.where('email', '=', req.body.email)
+				.then(user => {
+					res.json(user[0])
+				})
+				.catch(err => res.status(400).json('unable to get user'))
+		} else {
+			res.status(400).json('wrong credentials')
+		}
+	})
+	.catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	bcrypt.hash(password, null, null, function(err, hash) {
-		console.log(hash)
-	})
 
-	db('users')
-	.returning('*')
-	.insert({
-		name: name,
-		email: email,
-		joined: new Date()
-	})
-	.then(user => {
-		res.json(user[0])
+	const hash = bcrypt.hashSync(password);
+
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return db('users')
+			.returning('*')
+			.insert({
+				name: name,
+				email: loginEmail[0],
+				joined: new Date()
+			})
+			.then(user => {
+				res.json(user[0]);
+			})
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
 	})
 	.catch(err => res.status(400).json('unable to register'))
-	
 })
-
 
 app.get('/profile/:id', (req, res) => {
 	const {id} = req.params;
