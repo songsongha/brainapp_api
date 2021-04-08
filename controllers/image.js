2a905111c027bef591a7e3b2b442f4920051f772
Mykel <songsongha@gmail.com>
2021-04-08 15:38:00 -0600

    added validation check for blank forms on signin and register controllers, moved API key from front end to back end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+require('dotenv').config();
+
+const app = new Clarifai.App({
+    apiKey: process.env.API_KEY
+});
+
+const handleApiCall = (req, res) =>{
+	app.models
+	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data => {
+		res.json(data);
+	})
+	.catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
 	const {id} = req.body;
   	db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
   .catch(err => res.status(400).json('unable to get entries'))
 }
 module.exports = {
-	handleImage: handleImage
+	handleImage,
+	handleApiCall
 }
