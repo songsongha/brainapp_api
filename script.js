29de24e41cacef7b487ec188fd86a481ad02c6f9
Mykel <songsongha@gmail.com>
2021-05-04 15:10:00 -0600

    edited database connection info

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ require('dotenv').config();
 
 const db = knex ({
   client: 'pg',
-  connection: process.env.DATABASE_URL
+  connection: {
+  	connectionString : process.env.DATABASE_URL,
+  	ssl: {
+  		rejectUnauthorized: false
+  	}
+  }
 });
 
 
